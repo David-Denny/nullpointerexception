579a5df7757ed078b07fd94b6027653320d91c82
David Denny <dpwdenny@gmail.com>
2018-02-06 19:24:59 +0000

    Increase functionality for adding questions

--- addQuestion.js
+++ addQuestion.js
@@ -3,13 +3,30 @@
 	
 $(document).ready(function () {
 	"use strict";
+
+	var counter = 0;
 	
-	// assigns the input to variables
-	$("#questionForm").submit(function () {
+	/* 
+	Ideally the new rows would be loaded from a server but I don't have access to one and therefore the new questions cannot be retained after a page refresh or for different users but this is an example. 
+	*/
+	
+	$("#questionForm").submit(function (e) {
+		//prevents new row immediately disappearing
+		e.preventDefault();
 		
+		// assigns input to variables
 		var title = $("#enter-title").val(), question = $("#enter-question").val();
 		
-		alert(title);
-		alert(question);
+		// check input is not empty
+		if (title.length > 0 || question.length > 0) {
+			
+			$("#questionTable tbody").append("<tr class='" + counter + "'><td> 0 </td><td>" + title + "</td><td> 0 </td></tr>");
+			
+			counter++;
+		} else {
+			alert("Please do not submit an empty form.");
+		}
+
 	});
+	
 });
